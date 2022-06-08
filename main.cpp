676c3eb8beabe696ef427f57eb4f710dca0613b6
keke <243768648@qq.com>
2022-06-08 23:42:30 +0800

    feat: oh-my-dde致谢说明

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
     a.setStyle("chameleon");
     a.setWindowIcon(QIcon(":/icons/deepin/builtin/icons/grid_48px.svg"));
     a.setProductIcon(QIcon(":/icons/deepin/builtin/icons/grid_48px.svg"));
-    a.setApplicationDescription("ccc-app-manager是一款方便的第三方应用管理工具\n支持应用的安装，卸载，安装包提取等功能");
+    a.setApplicationDescription("ccc-app-manager是一款方便的第三方应用管理工具\n支持应用的安装，卸载，安装包提取等功能\n感谢yzzi开发了好用的oh-my-dde系统配置工具，我把工具的启动入口加到了本项目中");
     a.setQuitOnLastWindowClosed(true);//关闭主窗口后关闭关于
 
     // 保存窗口主题设置
