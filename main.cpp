dacf3c5887abe6fcb6432268030999c493a3c9a1
keke <243768648@qq.com>
2022-06-12 23:54:37 +0800

    feat: 单例运行模式

--- main.cpp
+++ main.cpp
@@ -22,6 +22,11 @@ int main(int argc, char *argv[])
     a.setProductIcon(QIcon(":/icons/deepin/builtin/icons/grid_48px.svg"));
     a.setApplicationDescription("ccc-app-manager是一款方便的第三方应用管理工具\n支持应用的安装，卸载，安装包提取等功能\n感谢yzzi开发了好用的oh-my-dde系统配置工具，我把工具的启动入口加到了本项目中");
     a.setQuitOnLastWindowClosed(true);//关闭主窗口后关闭关于
+    // 单例运行
+    if (!a.setSingleInstance("ccc-app-manager")) {
+        qInfo() << Q_FUNC_INFO << "ccc-app-manager is a single instance, exit!";
+        exit(0);
+    }
 
     // 保存窗口主题设置
     DApplicationSettings settings;
@@ -30,6 +35,11 @@ int main(int argc, char *argv[])
     w.show();
 
     Dtk::Widget::moveToCenter(&w);
+
+    QObject::connect(&a, &DApplication::newInstanceStarted, &a, [&w] {
+        w.showNormal();
+        w.activateWindow();
+    });
 
     return a.exec();
 }
