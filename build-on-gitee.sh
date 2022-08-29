73de2ac21f8035e65e48f6f0f19020b40d318054
shenmo <jifengshenmo@outlook.com>
2022-08-29 03:09:21 +0000

    update build-on-gitee.sh.
    Signed-off-by: shenmo <jifengshenmo@outlook.com>

--- build-on-gitee.sh
+++ build-on-gitee.sh
@@ -3,7 +3,9 @@ sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
 apt update
 export DEBIAN_FRONTEND=noninteractive
 echo "安装依赖..."
-apt install git curl fakeroot qtbase5-dev zlib1g-dev qt5-default libgsettings-qt-dev -y 
+#apt install git curl fakeroot qtbase5-dev zlib1g-dev qt5-default libgsettings-qt-dev -y 
+apt install libgsettings-qt-dev -y 
+exit
 git clone https://gitlink.org.cn/shenmo7192/dtk-old-bundle.git
 cd dtk-old-bundle
 apt install ./*.deb -y
