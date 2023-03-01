3fcf6283ec85b9b910d3baddb7331d384cb6e5e8
keke <243768648@qq.com>
2023-03-01 09:59:37 +0800

    feat: 使用gitee go的docker镜像构建debian 10环境，简化流水线脚本

--- build-on-gitee.sh
+++ build-on-gitee.sh
@@ -1,6 +1,6 @@
 # 换源
 echo "换源"
-echo "deb https://mirrors.huaweicloud.com/debian/ buster main contrib non-free" > /etc/apt/sources.list
+sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list
 apt-get update
 export DEBIAN_FRONTEND=noninteractive
 echo "安装依赖..."
@@ -8,21 +8,17 @@ apt-get install libgsettings-qt-dev -y
 apt-get install debhelper git curl fakeroot qtbase5-dev zlib1g-dev qt5-default  -y 
 git clone https://gitlink.org.cn/shenmo7192/dtk-old-bundle.git
 cd dtk-old-bundle
-apt install ./*.deb -y
+apt-get install ./*.deb -y
 cd ..
 rm -rf dtk-old-bundle
-
-cd gitee-go-build
 
 export version=${WORK_FLOW_ENV_PKG_VERSION}
 ./dpkg-buildpackage.sh
 
-mkdir target
+mkdir -p ../target
 
 output_file_path_list=$(find . -maxdepth 1 -type f -name "com.github.ccc-app-manager_*" -o -name "com.github.ccc-app-manager-dbgsym_*")
 for f in ${output_file_path_list}
 do
-    mv $f target
+    mv $f ../target
 done
-
-mv target /
