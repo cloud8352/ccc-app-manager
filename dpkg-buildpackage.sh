a9b4ed83095f491b37fb273721b6932292806c38
keke <24376868@qq.com>
2025-02-21 16:30:16 +0800

    feat: 调整构建格式

--- dpkg-buildpackage.sh
+++ dpkg-buildpackage.sh
@@ -24,7 +24,7 @@ EOF
 #更新info
 sed -i "s/\ \"version\":.*/\ \"version\": \"$version\",/g" debian/deepin_pkg_info/info
 
-dpkg-buildpackage -us -uc -b
+dpkg-buildpackage -us -uc
 
 cd ..
 rm -rf dpkg-build-tmp
